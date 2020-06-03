65e5dbe410aa9dcf91525568f73fe0090055da1b
imogollonh <52753345+imogollonh@users.noreply.github.com>
2020-06-03 13:11:36 -0600

    Mantenimientos

---
+++ Examen/Capa_Datos/Conexion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Odbc;
+
+namespace Capa_Datos
+{
+    public class Conexion
+    {
+        public OdbcConnection conexionbd()
+        {
+            OdbcConnection conn = new OdbcConnection("Dsn=dbfinal");// creacion de la conexion via ODBC
+
+            try
+            {
+                conn.Open();
+            }
+            catch (OdbcException ex)
+            {
+                Console.WriteLine("No se pudo realizar la conexión", ex);
+            }
+            return conn;
+        }
+    }
+}
+
